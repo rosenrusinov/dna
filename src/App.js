cf83979eb4efbedaedb1a2cedb6c20315f7a4d9b
nikol <kirova.dnastudio@gmail.com>
2020-06-08 23:44:05 +0200

    add custom domain

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
 
   render () {
     return (
-      <Router basename='/dna'>
+      <Router>
         <SimpleReactLightbox>
         <ScrollToTop>
         <Container className='p-0' fluid={true}>
